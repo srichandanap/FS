4315283bdabadb26b6ebfad55f3b51477494b69d
Sri Chandana <srichandana.p@robosoftin.com>
2022-11-21 14:55:28 +0530

    finall

--- my-app/src/components/placeDetails/placeDetails.tsx
+++ my-app/src/components/placeDetails/placeDetails.tsx
@@ -1,29 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './placeDetails.css';
+import { useSelector } from 'react-redux';
 
 const PlaceDetails = () => {
+
+    const [display, setDisplay] = useState<any>(false);
+    // const [searchedElement, setSearchedElement] = useState()
+
+    const places = useSelector((state: any) => state.local.places)
+
     return (
         <>
-            <div className="card">
-                <div className="left">
+            {places && places.nearby_restaurants && places.nearby_restaurants.map((place: any, i: any) => {
 
-                    <div className="fsq">
+                return (
+                    <div className='scroll'>
+                        <div className="card" >
+                            <div className="left">
 
-                        <img src={require("../../assets/icons/fsq.png")} alt="image" />
+                                <div className="fsq">
 
+                                    <img src={place && place.restaurant && place.restaurant.thumb} alt="image" />
+
+                                </div>
+                                <div className="restDetails">
+                                    <div className='restName'>{place && place.restaurant && place.restaurant.name && place.restaurant.name}</div>
+                                    <div className='cuisines'>{place && place.restaurant && place.restaurant.cuisines && place.restaurant.cuisines}</div>
+                                    <div className='address'>{place && place.restaurant && place.restaurant.location && place.restaurant.location.locality_verbose && place.restaurant.location.locality_verbose}</div>
+                                </div>
+                            </div>
+
+                            <div className='rate'>
+                                <div className='rating'>{place && place.restaurant && place.restaurant.user_rating && place.restaurant.user_rating.aggregate_rating && place.restaurant.user_rating.aggregate_rating}</div>
+                            </div>
+                        </div>
                     </div>
-                    <div className="restDetails">
-                        <div className='restName'>Plane Cafe</div>
-                        <div className='cuisines'>sandwiches</div>
-                        <div className='address'>manipal, karnataka</div>
-                    </div>
-                </div>
+                )
+            })
+            }
 
-                <div className='rate'>
-                    <div className='rating'>4</div>
-                </div>
-            </div>
-        </>
+        </ >
     )
 }
 
--- my-app/src/utils/api.tsx
+++ my-app/src/utils/api.tsx
@@ -1,26 +1,24 @@
-import React from 'react'
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
 const URL = 'https://developers.zomato.com/api/v2.1/geocode'
 
-const options = {
-  params:{
-    lat: '20.59',
-    lng: '78.96',
-  },
-  headers: {
-    Accept: "application/json",
-    "user-key": "c470e80941290e8b35355d10dcfb3e36",
-  }
-
-};
-
-export const getPlacesData = async () => {
+export const getPlacesData = async (localCoordinates: any) => {
 
   try {
 
-    const response = await axios.get(URL, options);
+    const response = await axios.get(URL + `?lat=${localCoordinates.lat
+      }&lon=${localCoordinates.lng}`, {
+      params: {
+        lat: localCoordinates.lat,
+        lng: localCoordinates.lng
+      },
+      headers: {
+        Accept: "application/json",
+        "user-key": "c470e80941290e8b35355d10dcfb3e36",
+      }
+
+    });
     // console.log(response);
     return response;
 
